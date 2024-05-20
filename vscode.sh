42812483be9c08c617e6bf4d3a597c467533e73a
wlstory <wlstory@gmail.com>
2024-05-20 13:10:28 -0500

    Update vscode.sh

--- vscode.sh
+++ vscode.sh
@@ -77,6 +77,6 @@ fi
 
 # Open VS Code to sign-in to extensions
 code .
-echo "Login to extensions (Copilot, Grammarly, etc) within VS Code."
+echo "Login to extensions (Copilot, etc) within VS Code."
 echo "Press enter to continue..."
 read
