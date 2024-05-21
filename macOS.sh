6ff7a2f3731f6cfeea95a2562828f27630597a50
wlstory <wlstory@gmail.com>
2024-05-21 17:56:21 -0500

    Update macOS.sh
    Removing Things to Do and moving to install.sh

--- macOS.sh
+++ macOS.sh
@@ -9,17 +9,6 @@ read
 # Set scroll as natural vs traditional - check if commenting reverts
 # defaults write NSGlobalDomain com.apple.swipescrolldirection -bool true && killall Finder
 
-# THINGS TO DO:
-# - Turn off password saving and auto-fill in browsers (Chrome, Safari, Brave, DuckDuckGo)
-# - Set HotCorners
-# - Set-up the Dock app order and icons on the Dock to include Applications
-# - Set Dock to Hide and Show with animation
-# - Check for OS Updates FIRST before any installations
-# - Learn and apply the Logitech Optiions Plus feature flags
-# - Check on ability to set/install Chrome browser extensions
-# - Establishing settings on Notifications
-# - Logitech Mouse settings? 
-
 # Get the absolute path to the image
 # *** REPLACE WITH MY IDEAL DESKTOP IMAGE ***
 #IMAGE_PATH="${HOME}/dotfiles/settings/Desktop.png"
