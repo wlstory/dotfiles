fa467ff244ab124d491217f04256b3a1dab70ddc
wlstory <wlstory@gmail.com>
2024-05-24 07:49:08 -0500

    Update vscode.sh
    removed commented code

--- vscode.sh
+++ vscode.sh
@@ -30,19 +30,6 @@ extensions=(
 #    Ruby Test Explorer
 #    ruby-rubocop
 
-# Install VS Code Extensions
-# extensions=(
-#    ms-python.python
-#    ms-python.pylint
-#    ms-python.vscode-pylance
-#    ms-python.debugpy
-#    GitHub.copilot
-#    ms-vscode.Theme-PredawnKit
-#    teabyii.ayu
-#    formulahendry.code-runner
-#    esbenp.prettier-vscode
-# )
-
 # Get a list of all currently installed extensions.
 installed_extensions=$(code --list-extensions)
 
